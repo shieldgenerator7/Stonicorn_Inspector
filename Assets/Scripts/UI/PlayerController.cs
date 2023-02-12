34bd57d130e4261860c86de599c2bc8a368882be
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-02-12 18:53:01 -0500

    Add stonicorn

--- Assets/Scripts/UI/PlayerController.cs
+++ Assets/Scripts/UI/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 2;
+
+    private Vector2 movePos = Vector2.zero;
+
+    public Vector2 Position
+    {
+        get => transform.position;
+        set => transform.position = value;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        movePos = Position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Position != movePos)
+        {
+            Position = Vector2.Lerp(Position, movePos, moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            movePos = pos;
+        }
     }
 }
