61d4e4bb2b9447a6bd54bbfbde2b9837df5c94a3
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-03-18 00:49:38 -0400

    Add camera controller

---
+++ Assets/Scripts/UI/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform follow;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - follow.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = follow.position + offset;
+    }
+}
