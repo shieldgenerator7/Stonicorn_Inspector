d8f9d0d1c45a167bdf3a8d8fc5298856d76a7269
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-07-31 06:36:15 -0400

    Remove unneeded functions from CustomMenu

--- Assets/Editor/CustomMenu.cs
+++ Assets/Editor/CustomMenu.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using UnityEditor;
 using UnityEngine.SceneManagement;
 using Debug = UnityEngine.Debug;
-using System.Linq;
 using UnityEditor.SceneManagement;
 using System;
 using System.Reflection;
@@ -64,77 +63,7 @@ public class CustomMenu
             FindInGO(childT.gameObject);
         }
     }
-
-    [MenuItem("SG7/Editor/Mechanics/Autosize BoxColliider2D to tiled sprite")]
-    public static void autosizeBC2DtoTiledSprite()
-    {
-        List<GameObject> gos = Selection.gameObjects.ToList();
-        gos = gos.FindAll(go =>
-            go.GetComponent<SpriteRenderer>()?.drawMode == SpriteDrawMode.Tiled
-            && go.GetComponent<BoxCollider2D>()
-            );
-        if (gos.Count == 0)
-        {
-            Debug.LogWarning("Select 1 or more gameobjects with both a SpriteRenderer in Tiled mode, and a BoxCollider2D");
-            return;
-        }
-        int changedCount = 0;
-        foreach (GameObject go in gos)
-        {
-            autosizeBC2DtoTiledSprite(go);
-            changedCount++;
-        }
-        Debug.Log($"Autosized {changedCount} BoxCollider2Ds to SpriteRenderer tiled size");
-    }
-    public static void autosizeBC2DtoTiledSprite(GameObject go)
-    {
-        SpriteRenderer sr = go.GetComponent<SpriteRenderer>();
-        BoxCollider2D bc2d = go.GetComponent<BoxCollider2D>();
-        bc2d.size = sr.size;
-        EditorUtility.SetDirty(bc2d);
-    }
-
-    [MenuItem("SG7/Editor/Mechanics/Auto-extend platform width")]
-    public static void autoextendPlatformWidth()
-    {
-        List<GameObject> gos = Selection.gameObjects.ToList();
-        gos = gos.FindAll(go =>
-            go.GetComponent<SpriteRenderer>()?.drawMode == SpriteDrawMode.Tiled
-            && go.GetComponent<BoxCollider2D>()
-            );
-        if (gos.Count == 0)
-        {
-            Debug.LogWarning("Select 1 or more gameobjects with both a SpriteRenderer in Tiled mode, and a BoxCollider2D");
-            return;
-        }
-        int changedCount = 0;
-        foreach (GameObject go in gos)
-        {
-            //Find left and right points
-            BoxCollider2D bc2d = go.GetComponent<BoxCollider2D>();
-            bc2d.enabled = false;
-            RaycastHit2D rch2dRight = Physics2D.Raycast(go.transform.position, go.transform.right);
-            Vector2 rightPos = rch2dRight.point;
-            RaycastHit2D rch2dLeft = Physics2D.Raycast(go.transform.position, -go.transform.right);
-            Vector2 leftPos = rch2dLeft.point;
-            bc2d.enabled = true;
-            //Move platform to center
-            go.transform.position = (rightPos + leftPos) / 2;
-            EditorUtility.SetDirty(go);
-            //Set size to distance between left and right points
-            SpriteRenderer sr = go.GetComponent<SpriteRenderer>();
-            Vector2 size = sr.size;
-            size.x = Vector2.Distance(leftPos, rightPos);
-            sr.size = size;
-            EditorUtility.SetDirty(sr);
-            //Update BoxCollider2D
-            autosizeBC2DtoTiledSprite(go);
-            //Update counter
-            changedCount++;
-        }
-        Debug.Log($"Auto-extended {changedCount} platform widths");
-    }
-
+    
     public static List<Scene> getLevelScenes(Func<Scene, bool> filter = null, bool reportFailures = false)
     {
         List<Scene> scenes = new List<Scene>();
@@ -156,46 +85,7 @@ public class CustomMenu
         return scenes;
     }
 
-    [MenuItem("SG7/Editor/Load or Unload Level Scenes %&S")]
-    public static void loadOrUnloadLevelScenes()
-    {
-        //Find out if all of the scenes are loaded
-        List<Scene> levels = getLevelScenes((s) => !String.IsNullOrEmpty(s.name), true);
-        bool allLoaded = allLevelScenesLoaded(levels);
-        //If any are unloaded, load them all.
-        //Else, unload them all.
-        loadAllLevelScenes(levels, !allLoaded);
-    }
-    public static void loadAllLevelScenes(List<Scene> levels, bool load)
-    {
-        //Load or unload all the level scenes
-        levels.ForEach(scene =>
-        {
-            if (!load)
-            {
-                //Unload
-                EditorSceneManager.CloseScene(scene, false);
-            }
-            else
-            {
-                //Load
-                try
-                {
-                    EditorSceneManager.OpenScene(
-                        scene.path,
-                        OpenSceneMode.Additive
-                        );
-                    SetExpanded(scene, false);
-                }
-                catch (ArgumentException ae)
-                {
-                    Debug.LogError($"scene load error ({scene.name}): {ae}");
-                }
-            }
-        });
-    }
-
-    //2020-12-09: copied from https://forum.unity.com/threads/how-to-collapse-hierarchy-scene-nodes-via-script.605245/#post-6551890
+       //2020-12-09: copied from https://forum.unity.com/threads/how-to-collapse-hierarchy-scene-nodes-via-script.605245/#post-6551890
     private static void SetExpanded(Scene scene, bool expand)
     {
         foreach (var window in Resources.FindObjectsOfTypeAll<SearchableEditorWindow>())
@@ -246,123 +136,19 @@ public class CustomMenu
         Debug.Log("Running all Pre-Build Tasks");
         //Setup
         EditorSceneManager.SaveOpenScenes();
-        List<Scene> levels = getLevelScenes(
-            (s) => !String.IsNullOrEmpty(s.name),
-            true
-            );
-        loadAllLevelScenes(levels, false);
-        loadAllLevelScenes(levels, true);
-        int waitCount = 0;
-        const int WAIT_LIMIT = 1000;
-        while (!allLevelScenesLoaded(levels))
-        {
-            new WaitForSecondsRealtime(0.1f);
-            waitCount++;
-            if (waitCount >= WAIT_LIMIT)
-            {
-                Debug.LogError("Waited longer than limit for scenes to load, continuing anyway");
-                break;
-            }
-        }
 
         //Checklist
-        bool keepScenesOpen = false;
-        //(new List<Func<bool>>()).ForEach(func => keepScenesOpen = keepScenesOpen || func);
-
-        keepScenesOpen = checkTiledHitBoxes() || keepScenesOpen;
+        bool problemFound = false;
+        //problemFound = functionCall() || problemFound;
 
         //Cleanup
         EditorSceneManager.SaveOpenScenes();
-        if (!keepScenesOpen)
-        {
-            loadAllLevelScenes(levels, false);
-        }
         //Finish
         Debug.Log("Finished all Pre-Build Tasks");
-        return keepScenesOpen;
+        return problemFound;
     }
 
-    static bool allLevelScenesLoaded(List<Scene> levels)
-        => levels.All(s => s.isLoaded);
 
-    [MenuItem("SG7/Build/Pre-Build/Check Tiled HitBoxes")]
-    public static bool checkTiledHitBoxes()
-    {
-        int changedCount = 0;
-        GameObject.FindObjectsOfType<SpriteRenderer>().ToList()
-            .FindAll(sr => sr.drawMode == SpriteDrawMode.Tiled)
-            .OrderBy(sr => sr.gameObject.scene.buildIndex)
-            .ThenBy(sr => sr.name).ToList()
-            .ForEach(sr =>
-            {
-                bool changedSR = false;
-                //
-                // Check for reasonable sprite size
-                //
-                Vector2 oldSRSize = sr.size;
-                Vector2 newSRSize = sr.size;
-                newSRSize.x = Mathf.Round(newSRSize.x * 100) / 100;
-                newSRSize.y = Mathf.Round(newSRSize.y * 100) / 100;
-                if (newSRSize != oldSRSize)
-                {
-                    sr.size = newSRSize;
-                    Debug.LogWarning(
-                        $"Changed {sr.name} sprite size " +
-                        $"from ({oldSRSize.x}, {oldSRSize.y}) " +
-                        $"to ({newSRSize.x}, {newSRSize.y}).",
-                        sr
-                        );
-                    changedSR = true;
-                }
-                //
-                // Check collider size
-                //
-                BoxCollider2D bc2d = sr.GetComponent<BoxCollider2D>();
-                if (bc2d)
-                {
-                    Vector2 oldSize = bc2d.size;
-                    Vector2 newSize = bc2d.size;
-                    //If tiled vertically,
-                    if (sr.size.y > sr.size.x * 2)
-                    {
-                        //Set collider height to match
-                        newSize.y = sr.size.y;
-                    }
-                    //Else: it's tiled horizontally, so
-                    else
-                    {
-                        //Set collider width to match
-                        newSize.x = sr.size.x;
-                    }
-                    if (newSize != oldSize)
-                    {
-                        bc2d.size = newSize;
-                        Debug.LogWarning(
-                            $"Changed {sr.name} collider size " +
-                            $"from ({oldSize.x}, {oldSize.y}) " +
-                            $"to ({newSize.x}, {newSize.y}).",
-                            sr
-                            );
-                        changedSR = true;
-                    }
-                }
-                if (changedSR)
-                {
-                    EditorUtility.SetDirty(sr);
-                    if (bc2d) { EditorUtility.SetDirty(bc2d); }
-                    EditorUtility.SetDirty(sr.gameObject);
-                    changedCount++;
-                }
-            });
-
-        if (changedCount > 0)
-        {
-            Debug.LogWarning(
-                $"Tiled sprites changes: Made {changedCount} changes."
-                );
-        }
-        return changedCount > 0;
-    }
 
     /// <summary>
     /// Recursively gather all files under the given path including all its subfolders.
@@ -425,9 +211,9 @@ public class CustomMenu
     public static void build(BuildTarget buildTarget, string extension)
     {
         string defaultPath = getDefaultBuildPath();
-        if (!System.IO.Directory.Exists(defaultPath))
+        if (!Directory.Exists(defaultPath))
         {
-            System.IO.Directory.CreateDirectory(defaultPath);
+            Directory.CreateDirectory(defaultPath);
         }
         //2017-10-19 copied from https://docs.unity3d.com/Manual/BuildPlayerPipeline.html
         // Get filename.
@@ -435,7 +221,9 @@ public class CustomMenu
 
         // User hit the cancel button.
         if (buildName == "")
+        {
             return;
+        }
 
         string path = buildName.Substring(0, buildName.LastIndexOf("/"));
         Debug.Log($"BUILDNAME: {buildName}");
@@ -456,9 +244,6 @@ public class CustomMenu
 
         // Build player.
         BuildPipeline.BuildPlayer(levels, buildName, buildTarget, BuildOptions.None);
-
-        // Copy a file from the project folder to the build folder, alongside the built game.
-        string resourcesPath = $"{path}/Assets/Resources";
 
         // Run the game (Process class from System.Diagnostics).
         Process proc = new Process();
